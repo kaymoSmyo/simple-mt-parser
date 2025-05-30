f73d289eb3c878475a7c9b038885932c835825d1
kaymoSmyo <aymsuq@gmail.com>
2025-05-31 02:29:02 +0900

    IsSource sをとるように変更

--- src/MTParser/Parser.hs
+++ src/MTParser/Parser.hs
@@ -3,39 +3,44 @@
 
 module MTParser.Parser (runParser, Parser, item, ParseError (..), mkParseError) where
 
-import Control.Applicative ( Alternative((<|>), empty) ) 
+import Control.Applicative (Alternative (empty, (<|>)))
 import Control.Monad.Except (ExceptT (ExceptT), MonadError (throwError), runExceptT)
 import Control.Monad.State (MonadState (get, put, state), State, runState)
+import MTParser.Source qualified as S
 
 data ParseError = Err String
     deriving (Show, Eq)
 
-newtype Parser a = P (ExceptT ParseError (State String) a)
+newtype Parser s a = P (ExceptT ParseError (State s) a)
     deriving
         ( Functor
         , Applicative
         , Monad
         , MonadError ParseError
-        , MonadState String
+        , MonadState s
         )
 
-instance Alternative Parser where
-    (<|>) :: Parser a -> Parser a -> Parser a
+instance (S.IsSource s) => Alternative (Parser s) where
+    (<|>) :: Parser s a -> Parser s a -> Parser s a
     p <|> q = P $ ExceptT $ state $ \input ->
         case runParser p input of
             (Left _, _) -> runParser q input
             ok -> ok
-    empty :: Parser a
+    empty :: Parser s a
     empty = throwError $ Err ""
 
 mkParseError :: String -> ParseError
 mkParseError = Err
 
-runParser :: Parser a -> String -> (Either ParseError a, String)
+runParser :: (S.IsSource s) => Parser s a -> s -> (Either ParseError a, s)
 runParser (P m) = runState (runExceptT m)
 
-item :: Parser Char
-item =
-    get >>= \case
-        [] -> throwError $ Err "Empty Input"
-        (x : xs) -> put xs >> pure x
+item :: (S.IsSource s) => Parser s Char
+item = do
+    s <- get
+    if S.isNull s
+        then throwError $ Err "Empty Input"
+        else
+            let x = S.head s
+                xs = S.tail s
+             in put xs >> pure x
