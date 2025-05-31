a71b83c83446e09d028915372fd3120ab27bea40
kaymoSmyo <aymsuq@gmail.com>
2025-05-31 10:58:04 +0900

    フォーマット

--- src/MTParser/Tools.hs
+++ src/MTParser/Tools.hs
@@ -13,55 +13,55 @@ import Data.Char (
 import MTParser.Parser (ParseError (Err), Parser, item)
 import MTParser.Source qualified as S
 
-sat :: S.IsSource s => (Char -> Bool) -> Parser s Char
+sat :: (S.IsSource s) => (Char -> Bool) -> Parser s Char
 sat p = do
     x <- item
     if p x
         then pure x
         else throwError . Err $ "not predicate at " ++ [x]
 
-digit :: S.IsSource s => Parser s Char
+digit :: (S.IsSource s) => Parser s Char
 digit = sat isDigit
 
-lower :: S.IsSource s => Parser s Char
+lower :: (S.IsSource s) => Parser s Char
 lower = sat isLower
 
-upper :: S.IsSource s => Parser s Char
+upper :: (S.IsSource s) => Parser s Char
 upper = sat isUpper
 
-letter :: S.IsSource s => Parser s Char
+letter :: (S.IsSource s) => Parser s Char
 letter = sat isAlpha
 
-alphanum :: S.IsSource s => Parser s Char
+alphanum :: (S.IsSource s) => Parser s Char
 alphanum = sat isAlphaNum
 
-char :: S.IsSource s => Char -> Parser s Char
+char :: (S.IsSource s) => Char -> Parser s Char
 char x = sat (== x)
 
-string :: S.IsSource s => String -> Parser s String
+string :: (S.IsSource s) => String -> Parser s String
 string [] = pure []
 string (x : xs) = do
     _ <- char x
     _ <- string xs
     pure (x : xs)
 
-ident :: S.IsSource s => Parser s String
+ident :: (S.IsSource s) => Parser s String
 ident = do
     x <- some lower
     xs <- many alphanum
     return (x ++ xs)
 
-nat :: S.IsSource s => Parser s Int
+nat :: (S.IsSource s) => Parser s Int
 nat = do
     xs <- some digit
     return (read xs)
 
-space :: S.IsSource s => Parser s ()
+space :: (S.IsSource s) => Parser s ()
 space = do
     _ <- many (sat isSpace)
     return ()
 
-int :: S.IsSource s => Parser s Int
+int :: (S.IsSource s) => Parser s Int
 int =
     do
         _ <- char '-'
@@ -69,7 +69,7 @@ int =
         return (-n)
         <|> nat
 
-token :: S.IsSource s => Parser s a -> Parser s a
+token :: (S.IsSource s) => Parser s a -> Parser s a
 token p = do
     space
     v <- p
