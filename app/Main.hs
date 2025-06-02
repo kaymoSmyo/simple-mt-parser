2614bd6e48c3de1c24f213a940c21854e92444ec
kaymoSmyo <aymsuq@gmail.com>
2025-06-02 18:19:51 +0900

    csvのレコードのパーサの作成

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,16 @@
 module Main (main) where
 
+import Control.Applicative
+import MTParser
+
 main :: IO ()
 main = do putStrLn "not impl"
+
+field :: Parser String String
+field = some alphanum
+
+connma :: Parser String Char
+connma = char ','
+
+record :: Parser String [String]
+record = (:) <$> field <*> many (connma *> field)
